037cf6c4e24df9ed95b0b26931735a08e3079e6a
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-05-28 18:52:59 +0300

    Remove extra build from configure

--- configure.sh
+++ configure.sh
@@ -24,9 +24,3 @@ else
   echo "Build directory 'build' not created. Configuration failed."
   exit 1
 fi
-
-# This command should be run to create the build folder.
-# After that, just run the build.sh script specifying
-# the target, build config (Debug/Release) and format (VST3/AU/Standalone).
-
-cmake -S . -B build
